ce4880046a2b0286e2bce8cd4b2733705bb6c145
Robert Rakhmatullin <shkipper.r@yandex.ru>
2024-05-23 23:43:50 +0300

    initial stage 1 commit

---
+++ src/app/ui/App.tsx
@@ -0,0 +1,34 @@
+import { Route, Routes } from "react-router-dom";
+import { MainPage } from "../../pages/main-page";
+import { Catalog } from "../../pages/catalog-page";
+import { About } from "../../pages/about-page";
+import { Product } from "../../pages/product-page";
+import { Cart } from "../../pages/cart-page";
+import { Error404_page } from "../../pages/error404-page";
+import { Layout } from "../../shared/ui/layout/layout";
+import { Contacts } from "../../pages/contacts";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* Главная страница */}
+        <Route index element={<MainPage />} />
+        {/* Каталог */}
+        <Route path="catalog" element={<Catalog />} />
+        {/* О магазине */}
+        <Route path="about" element={<About />} />
+        {/* Сраница товара */}
+        <Route path="product/:id" element={<Product />} />
+        {/* Корзина */}
+        <Route path="cart" element={<Cart />} />
+        {/* Контакты */}
+        <Route path="contacts" element={<Contacts />} />
+        {/* 404 */}
+        <Route path="*" element={<Error404_page />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/pages/error404-page.tsx
@@ -0,0 +1,3 @@
+export const Error404_page = () => {
+  return <div>Error404_page</div>;
+};
